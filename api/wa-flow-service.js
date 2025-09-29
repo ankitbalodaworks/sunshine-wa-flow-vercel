4d3da5eb11faf55ff2ff6a50cde8cd27071ee662
ankitbalodaworks <ankitbaloda.works@gmail.com>
2025-09-29 16:04:18 +0000

    chore: set Vercel runtime in function file and remove vercel.json

--- api/wa-flow-service.js
+++ api/wa-flow-service.js
@@ -1,6 +1,8 @@
 import { decryptFlowRequestBody, encryptFlowResponseBody } from '../lib/waCrypto.js';
 import { persistServiceSubmission } from '../lib/persist.js';
 
+export const config = { runtime: 'nodejs20.x' };
+
 export default async function handler(req, res) {
   try {
     if (req.method === 'GET') {
